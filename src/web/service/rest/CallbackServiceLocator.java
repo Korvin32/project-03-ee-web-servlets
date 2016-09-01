5b377c7231c1e5d97cf6581c76ba3532cc13de77
Kostyantyn Zagorodniy <korvin32@yahoo.com>
2016-09-01 07:32:41 +0200

    Initial project share - 2nd try

---
+++ src/web/service/rest/CallbackServiceLocator.java
@@ -0,0 +1,9 @@
+package web.service.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/callback")
+public class CallbackServiceLocator extends Application {
+
+}
---
+++ src/web/service/rest/OAuthCallbackService.java
@@ -0,0 +1,28 @@
+package web.service.rest;
+
+import javax.ejb.Stateless;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Stateless
+@Path("/oauth")
+public class OAuthCallbackService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(OAuthCallbackService.class);
+    
+    @GET
+    @Produces("application/json")
+    @Path("update/{id:[0-9][0-9]*}")
+    public Response update(@PathParam("id") Long id) {
+        LOG.info("--> update({}).", id);
+        LOG.info("<-- update().");
+        return Response.noContent().build();
+    }
+
+}
